121f1bde37b0cb09c2dd95a695af6111b0f36467
Alexander Wilkinson <alexanderwilkinson@Alexanders-MacBook-Pro.local>
2018-06-11 21:10:31 +0800

    updated mongoose loading added static files and tidying up the index file

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ const request = require('request');
 const app = express();
 require('dotenv').config()
 
+// DATABASE ---------------------------------------
+mongoose.connect(process.env.MONGOOSE_CONN);
 
+// MIDDLEWARE ------------------------------------
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
+// LOAD STATIC FILES ------------------------------
+app.use(express.static(__dirname + '/public'));
+
+// ROUTER ----------------------------------------
 require('./routes/index')(app);
 
+//SEVER ------------------------------------------
 app.listen(process.env.PORT);
